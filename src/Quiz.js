93a90e50e7dbc41a0bdc0e8eb16bdd8f340763a2
SamBlight <samuel.blight1@gmail.com>
2022-02-14 12:24:52 +1000

    Section 4 A

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, {Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json');
 
@@ -12,8 +13,11 @@ constructor(props) {
 }
 
 render() {
+    const isQuizEnd = quizData.quiz_questions[this.state.quiz_position - 1] === quizData.quiz_questions.length;
+
     return(
         <div>
+            {isQuizEnd && <QuizEnd/>}
             <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}> </QuizQuestion>
         </div>
         );
